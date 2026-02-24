1faff20e9b97c98594e4aa89beaba128801367c7
Luizrocha0 <luiz.rochahenrique2006@gmail.com>
2026-02-24 16:08:38 -0300

    Ajuste para deploy

--- src/firebaseConnection.js
+++ src/firebaseConnection.js
@@ -3,13 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEr4RA_DVRuwo7SRI1JQHRws-Reoru2pU",
-  authDomain: "curso-68dd2.firebaseapp.com",
-  projectId: "curso-68dd2",
-  storageBucket: "curso-68dd2.firebasestorage.app",
-  messagingSenderId: "338619301379",
-  appId: "1:338619301379:web:0efd6436eb16ce04909836",
-  measurementId: "G-W5D7TVRYDK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig)
